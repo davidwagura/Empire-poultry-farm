44ce11c5d7dd91d02c2c87b340f745b4f5447f78
David wanjohi <davidwagura83@gmail.com>
2024-12-10 07:28:58 +0300

    Chore: Modify footer page

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,8 +1,39 @@
 <template>
-    <footer class="bg-gray-800 text-white py-8">
-      <div class="container mx-auto text-center">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
-      </div>
-    </footer>
-  </template>
-  
+	<footer class="bg-[#a5c6c6] py-8">
+		<div class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 px-4">
+			<div>
+				<h3 class="text-2xl mb-4">Our Location</h3>
+				<p> Kenchic PLC Head Office, <br> Exsan Hse, Enterprise Rd <br> P.O. BOX 20052, 00200 Nairobi, <br> Kenya </p>
+			</div>
+			<div>
+				<h3 class="text-2xl mb-4">Customer Support</h3>
+				<p> HQ: +254 722 202 163 <br> Foods: +254 703 056 056 <br> Day Old Chicks: +254 703 056 055 <br> Email: <a href="mailto:pcservice@kenchic.com" class="hover:underline">pcservice@kenchic.com</a>
+					<br> Day Old Chicks: <a href="mailto:wakulima@kenchic.com" class="hover:underline">wakulima@kenchic.com</a>
+				</p>
+			</div>
+			<div>
+				<h3 class="text-2xl mb-4">Our Socials</h3>
+				<p>Follow Kenchic Foods</p>
+				<div class="flex space-x-4 my-2">
+					<a href="#" class="icon-twitter text-2xl border rounded-full py-2 px-4">
+						<i class="fa-sharp fa-solid fa-x"></i>
+					</a>
+					<a href="#" class="icon-facebook text-2xl border rounded-full py-2 px-3">
+						<i class="fab fa-facebook"></i>
+					</a>
+					<a href="#" class="icon-instagram text-2xl border rounded-full py-2 px-3">
+						<i class="fab fa-instagram"></i>
+					</a>
+					<a href="#" class="icon-youtube text-2xl border rounded-full py-2 px-3">
+						<i class="fab fa-youtube"></i>
+					</a>
+				</div>
+				<!-- <p>Follow Kenchic Day Old Chicks</p><div class="flex space-x-4 my-2"><a href="#" class="text-2xl"><i class="fab fa-twitter"></i></a><a href="#" class="text-2xl"><i class="fab fa-facebook"></i></a><a href="#" class="text-2xl"><i class="fab fa-instagram"></i></a><a href="#" class="text-2xl"><i class="fab fa-youtube"></i></a></div> -->
+			</div>
+		</div>
+		<div class="bg-[#e3d0ab] text-center py-4 mt-8">
+			<p class="text-sm">© 2024 Kenchic LTD</p>
+			<!-- <div class="flex justify-center space-x-4 mt-2"><a href="#" class="text-blue-600 hover:underline">Terms & Conditions</a><a href="#" class="text-blue-600 hover:underline">Privacy Policy</a><a href="#" class="text-blue-600 hover:underline">Cookie Policy</a></div> -->
+		</div>
+	</footer>
+</template>
